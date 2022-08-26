13b4c8920fce38c287817ebce721b6cd7beac604
anjana472 <110957441+anjana472@users.noreply.github.com>
2022-08-26 11:21:29 +0530

    Add files via upload

---
+++ sum of 2 num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+{
+    int num1,num2,sum;
+    printf("Enter two numbers");
+    scanf("%d%d",&num1,&num2);
+    sum=num1+num2;
+    printf("result is : %d",sum);
+
+    return 0;
+}
